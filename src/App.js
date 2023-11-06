82de3358565b7e638cdc1e2ba9419852918867ad
“Colin <muriithicolin98@gmail.com>
2023-11-06 20:34:35 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
